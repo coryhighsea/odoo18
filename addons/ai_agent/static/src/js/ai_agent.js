319f249c4044b740afe4f76ceabff64cf1fb486e
Coryhighsea <cjhisey@gmail.com>
2025-07-01 14:13:32 +0200

    wip

--- addons/ai_agent/static/src/js/ai_agent.js
+++ addons/ai_agent/static/src/js/ai_agent.js
@@ -1,7 +1,7 @@
 import { registry } from "@web/core/registry";
 import { useService } from "@web/core/utils/hooks";
 import { Component, useState, onMounted, useRef } from "@odoo/owl";
-import { patchSystray } from "@web/core/utils/patch_systray";
+// import { patchSystray } from "@web/core/utils/patch_systray";
 
 class AIAgentSystray extends Component {
     setup() {
