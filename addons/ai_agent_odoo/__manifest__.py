f144971c2553175a8819e18ea396f64c7e5f731a
Coryhighsea <cjhisey@gmail.com>
2025-07-03 18:52:36 +0200

    wip

--- addons/ai_agent_odoo/__manifest__.py
+++ addons/ai_agent_odoo/__manifest__.py
@@ -18,6 +18,8 @@
             'ai_agent_odoo/static/src/css/ai_agent.css',
             'ai_agent_odoo/static/src/js/ai_agent.js',
             'ai_agent_odoo/static/src/xml/ai_agent.xml',
+            'ai_agent_odoo/static/src/js/lib/marked.min.js',
+            'ai_agent_odoo/static/src/js/lib/dompurify.min,js',
         ],
     },
     'images': ['static/images/thumbnail.png'],
