e84bf793bdd5c505a24a05f8ab22a3b63be6d1b9
Coryhighsea <cjhisey@gmail.com>
2025-07-03 11:09:01 +0200

    wrong api-key variable

--- addons/ai_agent/static/src/js/ai_agent.js
+++ addons/ai_agent/static/src/js/ai_agent.js
@@ -47,7 +47,7 @@ class AIAgentWidget extends Component {
             // Make the call to your Python AI service
             const response = await fetch(`${config.ai_agent_url}/chat`, {
                 method: "POST",
-                headers: { "Content-Type": "application/json", "X-API-Key": config.ai_agent_api_key },
+                headers: { "Content-Type": "application/json", "api-Key": config.ai_agent_api_key },
                 body: JSON.stringify({
                     message: messageText,
                     conversation_history: conversationHistory,
